e0a870e7039cf7e7c47336dc35712f2737c5b6ca
Mst. Tahmina Jerin Arju <52301135+arju10@users.noreply.github.com>
2025-02-22 22:27:19 +0600

    print() statement

---
+++ print_statement.py
@@ -0,0 +1,6 @@
+'''
+print(): print() can be used to print something on the console in python.
+'''
+print("It will print string") # It will print string
+print(18 * 2) # It will print 36. Means mulplication of 18 and 2
+print("Division: ", 18 / 2) #  9. Means division of 18 and 2
